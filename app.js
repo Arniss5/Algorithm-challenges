a9689b98f3e0c8ebfa221d7aad37da634e0b73c4
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2022-06-21 08:32:20 +0100

    add new challenges

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// First Factorial
+// 1) First Factorial
 // Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer
 
 
@@ -13,7 +13,7 @@ function factorial(num) {
 }
 
 
-//Letter Capitalize
+// 2) Letter Capitalize
 
 // Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space.
 
@@ -29,5 +29,58 @@ function LetterCapitalize(str) {
     return capStr
 }
 
-// Alphabet Soup
+// 3) Alphabet Soup
 // Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
+
+//bubble sort
+// function alphabetSoup(str) {
+//     letterArray = str.split("")
+
+//     for(let i = 0; i < letterArray.length; i++){
+//         for(let j = 0; j < ( letterArray.length - i -1 ); j++){
+//             if(letterArray[j] > letterArray[j+1]){
+//                 let temp = letterArray[j]
+//                 letterArray[j] = letterArray[j + 1]
+//                 letterArray[j+1] = temp
+//             }
+//         }
+//     }
+//     return letterArray.join("")
+// }
+
+
+function alphabetSoup(str) {
+    letterArray = str.split("")
+
+    letterArray.sort()
+    return letterArray.join("")
+}
+
+
+
+// 4) Given an array of integers, find the sum of its elements.
+
+// For example, if the array [1,2,3,4] output would be 10
+
+function findSum(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    }
+
+    return sum
+}
+
+function findSum(arr) {
+    let sum = 0;
+
+    arr.forEach(num => {
+        sum += num
+    })
+
+    return sum
+}
+
+
+
